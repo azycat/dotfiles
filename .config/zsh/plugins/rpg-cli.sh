f05609d8d00a36fc7c61ffcd1ef7d2aa9b10ce8e
azycat <stinkybug3@gmail.com>
2024-02-05 16:39:32 -0800

    (arch zsh) tracking on separate branch

---
+++ .config/zsh/plugins/rpg-cli.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# An example setup to use the game in the bash shell. Check the guide for more options.
+
+# FIXME override this with your own path to rpg-cli
+RPG=/sbin/rpg-cli
+
+# Use the rpg as the command to do non fs related tasks such as print
+# status and buy items.
+rpg () {
+    $RPG "$@"
+    sync_rpg
+}
+
+ls () {
+    /sbin/ls "$@"
+    $RPG ls "$@"
+}
+
+# Try to move the hero to the given destination, and cd match the shell pwd
+# to that of the hero's location:
+# - the one supplied as parameter, if there weren't any battles
+# - the one where the battle took place, if the hero wins
+# - the home dir, if the hero dies
+cd () {
+    $RPG cd "$@"
+    sync_rpg
+}
+
+# Generate dungeon levels on the fly and look for treasures while moving down.
+#  Will start by creating dungeon/1 at the current directory, and /2, /3, etc.
+#  on subsequent runs.
+dn () {
+    current=$(basename $PWD)
+    number_re='^[0-9]+$'
+
+    if [[ $current =~ $number_re ]]; then
+        next=$(($current + 1))
+        command mkdir -p $next && cd $next && rpg ls
+    elif [[ -d 1 ]] ; then
+        cd 1 && rpg ls
+    else
+        command mkdir -p dungeon/1 && cd dungeon/1 && rpg ls
+    fi
+}
+
+# This helper is used to make the pwd match the tracked internally by the game
+sync_rpg () {
+    builtin cd "$($RPG pwd)"
+}
---
+++ .config/zsh/plugins/vim-ctrl-z.zsh
@@ -0,0 +1,11 @@
+vim-ctrl-z () {
+  if [[ $#BUFFER -eq 0 ]]; then
+    BUFFER="fg"
+    zle accept-line -w
+  else
+    zle push-input -w
+    zle clear-screen -w
+  fi
+}
+zle -N vim-ctrl-z
+bindkey '^Z' vim-ctrl-z
