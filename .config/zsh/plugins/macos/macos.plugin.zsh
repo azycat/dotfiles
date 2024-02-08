b1e689aa65114eeefa8500df3401ac5eec8faec4
Azy <56982188+azycat@users.noreply.github.com>
2024-02-07 18:56:05 -0800

    (zsh) clean up macos commands

--- .config/zsh/plugins/macos/macos.plugin.zsh
+++ .config/zsh/plugins/macos/macos.plugin.zsh
@@ -4,7 +4,7 @@
 0="${${(M)0:#/*}:-$PWD/$0}"
 
 # Open the current directory in a Finder window
-alias ofd='open_command $PWD'
+alias ofd='open $PWD'
 
 # Show/hide hidden files in the Finder
 alias showfiles="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
@@ -16,194 +16,28 @@ function btrestart() {
   sudo kextload -b com.apple.iokit.BroadcomBluetoothHostControllerUSBTransport
 }
 
-function _omz_macos_get_frontmost_app() {
-  osascript 2>/dev/null <<EOF
-    tell application "System Events"
-      name of first item of (every process whose frontmost is true)
-    end tell
-EOF
+# cd to current Finder directory
+# function cdf() {
+#   cd "$(pfd)"
+# }
+
+# cd to Finder
+cdf() {
+    target=`osascript -e 'tell application "Finder" to if (count of Finder windows) > 0 then get POSIX path of (target of front Finder window as text)'`
+    if [ "$target" != "" ]; then
+        cd "$target"; pwd
+    else
+        echo 'No Finder window found' >&2
+    fi
 }
 
-function tab() {
-  # Must not have trailing semicolon, for iTerm compatibility
-  local command="cd \\\"$PWD\\\"; clear"
-  (( $# > 0 )) && command="${command}; $*"
 
-  local the_app=$(_omz_macos_get_frontmost_app)
-
-  if [[ "$the_app" == 'Terminal' ]]; then
-    # Discarding stdout to quash "tab N of window id XXX" output
-    osascript >/dev/null <<EOF
-      tell application "System Events"
-        tell process "Terminal" to keystroke "t" using command down
-      end tell
-      tell application "Terminal" to do script "${command}" in front window
-EOF
-  elif [[ "$the_app" == 'iTerm' ]]; then
-    osascript <<EOF
-      tell application "iTerm"
-        set current_terminal to current terminal
-        tell current_terminal
-          launch session "Default Session"
-          set current_session to current session
-          tell current_session
-            write text "${command}"
-          end tell
-        end tell
-      end tell
-EOF
-  elif [[ "$the_app" == 'iTerm2' ]]; then
-    osascript <<EOF
-      tell application "iTerm2"
-        tell current window
-          create tab with default profile
-          tell current session to write text "${command}"
-        end tell
-      end tell
-EOF
-  elif [[ "$the_app" == 'Hyper' ]]; then
-    osascript >/dev/null <<EOF
-      tell application "System Events"
-        tell process "Hyper" to keystroke "t" using command down
-      end tell
-      delay 1
-      tell application "System Events"
-        keystroke "${command}"
-        key code 36  #(presses enter)
-      end tell
-EOF
-  else
-    echo "$0: unsupported terminal app: $the_app" >&2
-    return 1
-  fi
-}
-
-function vsplit_tab() {
-  local command="cd \\\"$PWD\\\"; clear"
-  (( $# > 0 )) && command="${command}; $*"
-
-  local the_app=$(_omz_macos_get_frontmost_app)
-
-  if [[ "$the_app" == 'iTerm' ]]; then
-    osascript <<EOF
-      -- tell application "iTerm" to activate
-      tell application "System Events"
-        tell process "iTerm"
-          tell menu item "Split Vertically With Current Profile" of menu "Shell" of menu bar item "Shell" of menu bar 1
-            click
-          end tell
-        end tell
-        keystroke "${command} \n"
-      end tell
-EOF
-  elif [[ "$the_app" == 'iTerm2' ]]; then
-    osascript <<EOF
-      tell application "iTerm2"
-        tell current session of first window
-          set newSession to (split vertically with same profile)
-          tell newSession
-            write text "${command}"
-            select
-          end tell
-        end tell
-      end tell
-EOF
-  elif [[ "$the_app" == 'Hyper' ]]; then
-    osascript >/dev/null <<EOF
-    tell application "System Events"
-      tell process "Hyper"
-        tell menu item "Split Vertically" of menu "Shell" of menu bar 1
-          click
-        end tell
-      end tell
-      delay 1
-      keystroke "${command} \n"
-    end tell
-EOF
-  else
-    echo "$0: unsupported terminal app: $the_app" >&2
-    return 1
-  fi
-}
-
-function split_tab() {
-  local command="cd \\\"$PWD\\\"; clear"
-  (( $# > 0 )) && command="${command}; $*"
-
-  local the_app=$(_omz_macos_get_frontmost_app)
-
-  if [[ "$the_app" == 'iTerm' ]]; then
-    osascript 2>/dev/null <<EOF
-      tell application "iTerm" to activate
-
-      tell application "System Events"
-        tell process "iTerm"
-          tell menu item "Split Horizontally With Current Profile" of menu "Shell" of menu bar item "Shell" of menu bar 1
-            click
-          end tell
-        end tell
-        keystroke "${command} \n"
-      end tell
-EOF
-  elif [[ "$the_app" == 'iTerm2' ]]; then
-    osascript <<EOF
-      tell application "iTerm2"
-        tell current session of first window
-          set newSession to (split horizontally with same profile)
-          tell newSession
-            write text "${command}"
-            select
-          end tell
-        end tell
-      end tell
-EOF
-  elif [[ "$the_app" == 'Hyper' ]]; then
-    osascript >/dev/null <<EOF
-    tell application "System Events"
-      tell process "Hyper"
-        tell menu item "Split Horizontally" of menu "Shell" of menu bar 1
-          click
-        end tell
-      end tell
-      delay 1
-      keystroke "${command} \n"
-    end tell
-EOF
-  else
-    echo "$0: unsupported terminal app: $the_app" >&2
-    return 1
-  fi
-}
-
-function pfd() {
-  osascript 2>/dev/null <<EOF
-    tell application "Finder"
-      return POSIX path of (insertion location as alias)
-    end tell
-EOF
-}
-
-function pfs() {
-  osascript 2>/dev/null <<EOF
-    set output to ""
-    tell application "Finder" to set the_selection to selection
-    set item_count to count the_selection
-    repeat with item_index from 1 to count the_selection
-      if item_index is less than item_count then set the_delimiter to "\n"
-      if item_index is item_count then set the_delimiter to ""
-      set output to output & ((item item_index of the_selection as alias)'s POSIX path) & the_delimiter
-    end repeat
-EOF
-}
-
-function cdf() {
-  cd "$(pfd)"
-}
-
+# pushd to the current finder directory
 function pushdf() {
   pushd "$(pfd)"
 }
 
+# returns the current xcode project directory
 function pxd() {
   dirname $(osascript 2>/dev/null <<EOF
     if application "Xcode" is running then
@@ -215,14 +49,17 @@ EOF
 )
 }
 
+# cd to the current xcode project directory
 function cdx() {
   cd "$(pxd)"
 }
 
+# quick-look a specified file
 function quick-look() {
   (( $# > 0 )) && qlmanage -p $* &>/dev/null &
 }
 
+# open man pages in Preview app
 function man-preview() {
   [[ $# -eq 0 ]] && >&2 echo "Usage: $0 command1 [command2 ...]" && return 1
 
